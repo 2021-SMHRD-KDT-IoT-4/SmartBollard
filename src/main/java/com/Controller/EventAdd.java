47e2afbc77116d3832a05ad6188467079c56f402
smhrd <smhrd@DESKTOP-6IHAUI2>
2021-06-22 17:16:10 +0900

    ddd

---
+++ src/main/java/com/Controller/EventAdd.java
@@ -0,0 +1,45 @@
+package com.Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.Model.eventDAO;
+import com.Model.eventDTO;
+
+
+@WebServlet("/EventAdd")
+public class EventAdd extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+	String ename = request.getParameter("ename");
+	String day = request.getParameter("day");
+	String host = request.getParameter("host");
+	String etime = request.getParameter("etime");
+	int people = Integer.parseInt(request.getParameter("people"));
+	String sstreet = request.getParameter("sstreet");
+	String estreet = request.getParameter("estreet");
+	
+	
+	eventDTO dto = new eventDTO(ename, day, host, etime, people, sstreet, estreet);
+	eventDAO eventdao = new eventDAO();
+	int cnt = eventdao.eventAdd(dto);
+	
+	if(cnt>0) {
+		System.out.println("축제추가 성공");
+	}else {
+		System.out.println("축제추가 실패");
+	}
+	
+	
+	response.sendRedirect("SmartBolad.jsp#event");
+	
+	
+	}
+	
+}
---
+++ src/main/java/com/Model/eventDTO.java
@@ -0,0 +1,112 @@
+package com.Model;
+
+public class eventDTO {
+
+	private String ename;
+	private String day;
+	private String host;
+	private String etime;
+	private int people;
+	private String sstreet;
+	private String estreet;
+	
+	
+	
+	public eventDTO(String ename, String day, String host, String etime, int people, String sstreet, String estreet) {
+		super();
+		this.ename = ename;
+		this.day = day;
+		this.host = host;
+		this.etime = etime;
+		this.people = people;
+		this.sstreet = sstreet;
+		this.estreet = estreet;
+	}
+
+
+
+	public String getEname() {
+		return ename;
+	}
+
+
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+
+
+	public String getDay() {
+		return day;
+	}
+
+
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+
+
+	public String getHost() {
+		return host;
+	}
+
+
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+
+
+	public String getEtime() {
+		return etime;
+	}
+
+
+
+	public void setEtime(String etime) {
+		this.etime = etime;
+	}
+
+
+
+	public int getPeople() {
+		return people;
+	}
+
+
+
+	public void setPeople(int people) {
+		this.people = people;
+	}
+
+
+
+	public String getSstreet() {
+		return sstreet;
+	}
+
+
+
+	public void setSstreet(String sstreet) {
+		this.sstreet = sstreet;
+	}
+
+
+
+	public String getEstreet() {
+		return estreet;
+	}
+
+
+
+	public void setEstreet(String estreet) {
+		this.estreet = estreet;
+	}
+	
+	
+	
+}
